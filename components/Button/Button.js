efc52f20d42d2f39232f5b9fcc572d7b15057ebb
Gaspar Gonzalez <gaspargonzalez@MacBook-Air-de-Gaspar.local>
2022-07-22 22:14:39 -0300

    pagina principal

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -5,38 +5,28 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 const ButtonCheck = () => {
     return(
         <TouchableOpacity style={styles.button}>
-            <View >
                    <Text style={styles.btn}>Iniciar Sesion</Text>
-            </View>
-         
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     button: {
-        width: '70%',
-        height: 70,
-        backgroundColor: 'blue',
-        marginTop: 50,
+        backgroundColor: '#00bfff',
+        height: 50,
+        width: '60%',
         justifyContent: 'center',
         alignItems: 'center',
-        alignContent: 'center',
+        borderRadius: 10,
+        marginTop: 50,
         
         
     },
     btn: {
-        width: '100%',
-        height: '100%',
         fontSize: 20,
-        backgroundColor: '#00bfff',
-        height: '100%',
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        color: 'white',
+        color: '#fff',
         fontWeight: 'bold',
-        alignContent: 'center',
-        alignItems: 'center',
+        
         
 
         
