35dbfe954b106ea23973a8fca7ace592be865e32
Gaspar Gonzalez <gaspargonzalez@MacBook-Air-de-Gaspar.local>
2022-07-25 19:50:36 -0300

    navegacion

---
+++ pages/MenuApp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground , TextInput} from "react-native";
+
+
+const MenuApp = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+
+export default MenuApp;
